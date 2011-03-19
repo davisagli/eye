c78dec326910417305328159da105aa075436dd1
David Glick <dglick@gmail.com>
2011-03-19 15:39:10 -0700

    traverse BTrees; handle a problem with non-dict Broken states

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,16 @@
 Changelog
 =========
 
+1.0b2 - unreleased
+------------------
+
+* Handle case where ZODB state is not a mapping.
+  [davisagli]
+
+* Make it possible to browse ZODB BTrees (as long as they're not too big,
+  anyway).
+  [davisagli]
+
 1.0b1 - 2011-03-19
 ------------------
 
--- eye/models.py
+++ eye/models.py
@@ -1,6 +1,7 @@
 import collections
 import inspect
 
+
 PRIMITIVES = set([int, bool, str, unicode, type(None)])
 
 
@@ -10,19 +11,29 @@ class Node(object):
         self.context = context
     
     def _dict(self):
-        if type(self.context) in PRIMITIVES:
+        d = None
+        
+        type_ = type(self.context)
+        if type_ in PRIMITIVES:
             d = {}
+        elif type_.__name__.endswith('BTree'):
+            # ZODB BTrees
+            d = dict((str(k), v) for k, v in self.context.iteritems())
         elif isinstance(self.context, collections.Mapping):
             d = self.context
         elif isinstance(self.context, collections.Iterable):
             d = dict((str(i), v) for i, v in enumerate(self.context))
         elif hasattr(self.context, '__Broken_state__'):
             # ZODB
-            d = self.context.__Broken_state__
-        else:
+            if isinstance(self.context.__Broken_state__, collections.Mapping):
+                d = self.context.__Broken_state__
+            else:
+                d = None
+        
+        if d is None:
             d = dict(inspect.getmembers(self.context))
         
-        return dict((k, Node(v)) for k,v in d.items())
+        return dict((k, Node(v)) for k,v in d.iteritems())
 
     def __getitem__(self, name):
         return self._dict()[name]
