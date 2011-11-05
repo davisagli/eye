4007b6b490ac667c8423c6cc789b303e93f9d03d
David Glick <dglick@gmail.com>
2011-11-05 15:42:41 -0700

    vb

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='eye',
-      version='1.0',
+      version='1.0.1dev',
       description='A generic web-based browser of ZODB databases.',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
